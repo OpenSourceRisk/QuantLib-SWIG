8ab9fbdafbc4dbd70c5f8ee12d9b91b93b8f378f
Roland Lichters <roland.lichters@acadia.inc>
2022-02-03 10:43:57 +0100

    QPR-10590 add deepUpdate to new rate helpers

--- ql/experimental/termstructures/basisswapratehelpers.cpp
+++ ql/experimental/termstructures/basisswapratehelpers.cpp
@@ -106,7 +106,8 @@ namespace QuantLib {
     }
 
     Real IborIborBasisSwapRateHelper::impliedQuote() const {
-        swap_->recalculate();
+        //swap_->recalculate();
+        swap_->deepUpdate();
         return - (swap_->NPV() / swap_->legBPS(0)) * 1.0e-4;
     }
 
@@ -185,7 +186,8 @@ namespace QuantLib {
     }
 
     Real OvernightIborBasisSwapRateHelper::impliedQuote() const {
-        swap_->recalculate();
+        // swap_->recalculate();
+        swap_->deepUpdate();
         return - (swap_->NPV() / swap_->legBPS(0)) * 1.0e-4;
     }
 
