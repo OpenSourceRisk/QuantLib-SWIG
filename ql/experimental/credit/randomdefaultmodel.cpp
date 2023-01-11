4f7052f0cf68135cea5095f06f1795202609daa9
Peter Caspers <peter.caspers@acadia.inc>
2023-01-11 11:28:05 +0100

    QPR-11360 add some comments, better error message, code formatting

--- ql/experimental/credit/randomdefaultmodel.cpp
+++ ql/experimental/credit/randomdefaultmodel.cpp
@@ -35,7 +35,8 @@ namespace QuantLib {
             Root(Handle<DefaultProbabilityTermStructure> dts, Real pd)
             : dts_(std::move(dts)), pd_(pd) {}
             Real operator()(Real t) const {
-                QL_REQUIRE (t >= 0.0, "t < 0");
+                QL_REQUIRE(t >= 0.0, "GaussianRandomDefaultModel: internal error, t < 0 ("
+                                         << t << ") during root searching.");
                 return dts_->defaultProbability(t, true) - pd_;
             }
           private:
@@ -73,17 +74,20 @@ namespace QuantLib {
             Real p = CumulativeNormalDistribution()(y);
 
             if (dts->defaultProbability(tmax) < p)
-                pool_->setTime(name, tmax+1);
+                pool_->setTime(name, tmax + 1);
             else {
-                 try {
-                     Brent brent;
-                     brent.setLowerBound(0.0);
-                     brent.setUpperBound(tmax);
-                     pool_->setTime(name, brent.solve(Root(dts,p),accuracy_, tmax/2.0, 1));
-                 }
-                 catch(...){
-                     pool_->setTime(name, Bisection().solve(Root(dts,p), accuracy_, tmax/2.0, 0.0, tmax));
-                 }
+                // we know there is a zero of f(t) = dts->defaultProbability(t) - p in [0, tmax]
+                try {
+                    // try bracketing the root and find it with Brent
+                    Brent brent;
+                    brent.setLowerBound(0.0);
+                    brent.setUpperBound(tmax);
+                    pool_->setTime(name, brent.solve(Root(dts, p), accuracy_, tmax / 2.0, 1.0));
+                } catch (...) {
+                    // if Brent fails, use Bisection, this is guaranteed to find the root
+                    pool_->setTime(
+                        name, Bisection().solve(Root(dts, p), accuracy_, tmax / 2.0, 0.0, tmax));
+                }
             }
         }
     }
